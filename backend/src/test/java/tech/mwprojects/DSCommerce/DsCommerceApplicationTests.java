9ec4baf4da2bf198a1aafd2b3bb5a189fe295560
Marcos Willian <willian.marcos7@hotmail.com>
2024-04-07 20:57:27 -0300

    project created

---
+++ backend/src/test/java/tech/mwprojects/DSCommerce/DsCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.mwprojects.DSCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
