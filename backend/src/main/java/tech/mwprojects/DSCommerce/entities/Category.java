48a3ccd7c17aab80a577fb18a0f91ea3faf2ef69
Marcos Willian <willian.marcos7@hotmail.com>
2024-04-07 23:05:17 -0300

    category and product many to many

---
+++ backend/src/main/java/tech/mwprojects/DSCommerce/entities/Category.java
@@ -0,0 +1,71 @@
+package tech.mwprojects.DSCommerce.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	
+	@ManyToMany(mappedBy = "categories")
+	private List<Product> products = new ArrayList<>();
+	
+	public Category() {}
+	
+	public Category(Integer id, String name) {
+		this.name = name;
+		this.id = id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public List<Product> getProducts(){
+		return products;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+}
