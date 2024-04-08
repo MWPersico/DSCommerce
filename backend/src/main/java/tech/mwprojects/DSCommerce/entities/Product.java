2baeba352a3cdaf7e1f6edf5fd756c43c779acdc
Marcos Willian <willian.marcos7@hotmail.com>
2024-04-07 23:13:44 -0300

    fix: unique and text columns

--- backend/src/main/java/tech/mwprojects/DSCommerce/entities/Product.java
+++ backend/src/main/java/tech/mwprojects/DSCommerce/entities/Product.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +24,7 @@ public class Product implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	@Column(columnDefinition ="TEXT")
 	private String description;
 	private Double price;
 	private String imageUrl;
