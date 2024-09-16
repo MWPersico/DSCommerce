25539825a011d52b59eb3f71eca94e8cfe5c3738
Marcos Persico <willian.marcos7@hotmail.com>
2024-09-15 22:55:26 -0300

    products custom response

--- backend/src/main/java/tech/mwprojects/DSCommerce/controllers/ProductController.java
+++ backend/src/main/java/tech/mwprojects/DSCommerce/controllers/ProductController.java
@@ -1,14 +1,18 @@
 package tech.mwprojects.DSCommerce.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import tech.mwprojects.DSCommerce.dto.ProductDTO;
 import tech.mwprojects.DSCommerce.services.ProductService;
@@ -20,19 +24,27 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    // parâmetros de URL podem ser passados para paginação: /products?size=10&page=2&sort=name,desc
+    // Parâmetros de URL podem ser passados para paginação: /products?size=10&page=2&sort=name,desc
     @GetMapping
-    public Page<ProductDTO> findAll(Pageable pageable){
-        return service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
+        return ResponseEntity.ok(service.findAll(pageable)) ;
     }
 
     @GetMapping(value="/{id}")
-    public ProductDTO findById(@PathVariable Integer id){
-        return service.findById(id);
+    public ResponseEntity<ProductDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok(service.findById(id));
     }
     
     @PostMapping
-    public ProductDTO create(@RequestBody ProductDTO product) {
-    	return service.create(product);
+    public ResponseEntity<ProductDTO> create(@RequestBody ProductDTO product) {
+    	ProductDTO created = service.create(product);
+    	URI uri = ServletUriComponentsBuilder
+    			.fromCurrentRequest()
+    			.path("/{id}")
+    			.buildAndExpand(created.getId())
+    			.toUri();
+    	
+    	// Retorna o produto criado e o header Location com URL do recurso
+    	return ResponseEntity.created(uri).body(created);
     }
 }
