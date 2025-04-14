82d5b95902c4f0262391f382342e7096da8e7255
MWPersico <willian.marcos7@hotmail.com>
2025-04-14 20:55:40 -0300

    ResourceServer and AuthorizationServer implemented and configured

--- backend/src/main/java/tech/mwprojects/DSCommerce/DsCommerceApplication.java
+++ backend/src/main/java/tech/mwprojects/DSCommerce/DsCommerceApplication.java
@@ -2,22 +2,11 @@ package tech.mwprojects.DSCommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class DsCommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DsCommerceApplication.class, args);
-	}
-
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(AbstractHttpConfigurer::disable);
-		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-		return http.build();
 	}
 }
--- backend/src/main/java/tech/mwprojects/DSCommerce/controllers/ProductController.java
+++ backend/src/main/java/tech/mwprojects/DSCommerce/controllers/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -30,7 +31,8 @@ public class ProductController {
     public ResponseEntity<ProductDTO> findById(@PathVariable Integer id){
         return ResponseEntity.ok(service.findById(id));
     }
-    
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<ProductDTO> create(@Valid @RequestBody ProductDTO product) {
     	ProductDTO created = service.create(product);
