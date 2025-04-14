027e1f077ff072487753e24abe6ebc4d421b6b76
MWPersico <willian.marcos7@hotmail.com>
2025-04-14 19:15:34 -0300

    user-role model implemented

---
+++ backend/src/main/java/tech/mwprojects/DSCommerce/entities/Role.java
@@ -0,0 +1,54 @@
+package tech.mwprojects.DSCommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="tb_role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String Authority;
+
+    @ManyToMany
+    @JoinTable(
+            name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private final Set<User> users = new HashSet<>();
+
+    public Role(){}
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        Authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return Authority;
+    }
+
+    public void setAuthority(String authority) {
+        Authority = authority;
+    }
+
+    public Set<User> getUsers(){
+        return users;
+    }
+
+    public void addUser(User user){
+        users.add(user);
+    }
+}
--- backend/src/main/java/tech/mwprojects/DSCommerce/entities/User.java
+++ backend/src/main/java/tech/mwprojects/DSCommerce/entities/User.java
@@ -2,17 +2,9 @@ package tech.mwprojects.DSCommerce.entities;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="tb_user")
@@ -31,7 +23,9 @@ public class User implements Serializable{
 	
 	@OneToMany(mappedBy = "client") // One User to many Orders
 	private List<Order> orders = new ArrayList<>();
-	//private String[] roles;
+
+	@ManyToMany(mappedBy = "users")
+	private Set<Role> roles = new HashSet<>();
 	
 	public User() {}
 	
@@ -94,6 +88,10 @@ public class User implements Serializable{
 	
 	public List<Order> getOrders(){
 		return orders;
+	}
+
+	public Set<Role> getRoles(){
+		return roles;
 	}
 
 	@Override
