57fa6f0f501868e4c84bb22d58ec111dc7efc214
MWPersico <willian.marcos7@hotmail.com>
2025-04-14 19:19:11 -0300

    spring security added and SecurityFilterChain bypass

--- backend/src/main/java/tech/mwprojects/DSCommerce/DsCommerceApplication.java
+++ backend/src/main/java/tech/mwprojects/DSCommerce/DsCommerceApplication.java
@@ -2,6 +2,10 @@ package tech.mwprojects.DSCommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class DsCommerceApplication {
@@ -10,4 +14,10 @@ public class DsCommerceApplication {
 		SpringApplication.run(DsCommerceApplication.class, args);
 	}
 
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable);
+		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+		return http.build();
+	}
 }
